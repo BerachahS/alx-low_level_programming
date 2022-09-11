eb5691f3359fdfd156308bab76eb139c490ef7c2
BerachahS <berachahshakes938@gmail.com>
2022-09-11 07:14:55 -0700

    added corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,9 @@
  *
  *Return: 0
  */
-int main ()
-
+int main()
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	return(0);
 }
