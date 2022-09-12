773c3b420a004c938a3fcc3a31d909d31f919980
BerachahS <berachahshakes938@gmail.com>
2022-09-12 08:37:07 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main -prints character
+ *
+ *Description:the printf function prints a string
+ *:with proper grammar, but the outcome is a piece of art
+ *Return:0
+ */
+
+ int main(void)
+ {
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return(0);
+}
