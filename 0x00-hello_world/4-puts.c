a214438d126f75414790e4a6d6127bd8436194e4
BerachahS <berachahshakes938@gmail.com>
2022-09-12 14:08:23 -0700

    added more changes to code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Prints the string in the puts function
+ *main - prints string in the puts function 
  *
- * Description: using the main function,
- * this program prints : programming is like building a multilingual puzzle,
- * ALways returns : 0
+ *Description: using the main function,
+ *this progam prints the string in the puts function,
+ *Return: 0
  */
 int main(void)
 {
