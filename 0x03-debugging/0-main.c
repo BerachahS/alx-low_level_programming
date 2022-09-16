5381b93946b0312d3d25b3ca8d91e5bc31d31741
BerachahS <berachahshakes938@gmail.com>
2022-09-15 17:58:47 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints postive or negative integer
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
