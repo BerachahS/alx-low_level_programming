b8f13b9947a14b052bb6247ce03c8fd1442d3aca
BerachahS <berachahshakes938@gmail.com>
2022-09-20 05:11:55 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * puts2 - prints character of a string starting with the
+ * first one
+ * @str: str parameter
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
