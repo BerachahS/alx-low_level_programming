8863b749f8e34110c283f52e4663fee9f934b767
BerachahS <berachahshakes938@gmail.com>
2022-09-21 15:55:39 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - a function that appends characters
+ *
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ *
+ * Return: string
+ */
+char *strncat(char *dest, char *src, int n);
+{
+	strncat(dest, src, n);
+
+	return (dest);
+}
