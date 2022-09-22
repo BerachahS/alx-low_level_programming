888738fcd525b7311907c0545a6a8b09d253ac60
BerachahS <berachahshakes938@gmail.com>
2022-09-21 17:20:39 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes strings
+ * @x: string to be convrted to uppercase
+ *
+ * Return: string
+ */
+char *cap_string(char *x)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
+
+	int len = 13;
+	int a = 0, i;
+
+	while (x[a])
+	{
+		i = 0;
+
+		while (x[a])
+		{
+			i = 0;
+
+			while (i < len)
+			{
+				if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				s[a] = s[a] - 32;
+				i++;
+			}
+			a++;
+		}
+	}
+	return (x);
+}
